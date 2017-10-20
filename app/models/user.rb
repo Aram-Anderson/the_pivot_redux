a49012e85b9eda971fe81324cce1d3f7372e2831
Aram Anderson <aram.j.anderson@gmail.com>
2017-10-20 08:59:27 -0600

    Fix seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
-  has_many :user_roles
+  has_many :user_roles, dependent: :delete_all
   has_many :roles, through: :user_roles
 
   validates :first_name, :last_name, :password, presence: true
