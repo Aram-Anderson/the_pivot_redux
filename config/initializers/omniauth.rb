1d6201f334f3fd389a02fd67889fed88f86a2c53
sidewinder2020 <sdwinder1@gmail.com>
2017-10-22 16:03:59 -0600

    twitter login page working, fixed figaro syntax

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,5 +6,5 @@ end
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["SPOTIFY_SECRET_ID"]
+  provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_SECRET_ID"]
 end
