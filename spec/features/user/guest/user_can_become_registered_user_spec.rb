1ee4bb8b2a72debb0f069803f4aeee918521dae9
snayrouz <samuel.nayrouz@gmail.com>
2017-10-23 13:37:17 -0600

    Add user permissions

--- spec/features/user/guest/user_can_become_registered_user_spec.rb
+++ spec/features/user/guest/user_can_become_registered_user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 feature "Guest User Becomes Registered User" do
   context "they can sign up to become a registered user" do
-    visit '/si'
+    visit '/login'
+
+    
 
 
   end
