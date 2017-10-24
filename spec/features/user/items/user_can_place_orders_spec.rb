6f15f6a71fc5a4481467815ad4c20178956fc8a6
Aram Anderson <aram.j.anderson@gmail.com>
2017-10-23 20:15:06 -0600

    spec/features/user/items/user_can_place_orders_spec.rb fixed

--- spec/features/user/items/user_can_place_orders_spec.rb
+++ spec/features/user/items/user_can_place_orders_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.feature "User can place an order" do
   it "and see the message 'order was successfully placed'" do
     Role.create([{role: 0}, {role: 1}])
-    user = User.create(first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
+    user = User.create(first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address", phone: "+12023339999")
+    store = create(:store)
+    item1 = create(:item, store: store)
 
-    create_items
 
-    visit items_path
+    visit store_items_path(store.slug)
 
     click_on "Add to cart"
 
