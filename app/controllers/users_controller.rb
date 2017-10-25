553f74a7b2c6c9e8435a3e7bf50df73f603b711f
sidewinder2020 <sdwinder1@gmail.com>
2017-10-24 18:09:34 -0600

    made a few small changes to strong_params, etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       ConfirmationSender.send_confirmation(@user)
-      redirect_to new_confirmation_path
+      redirect_to root_path
     else
       render :new
     end
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :address, :phone)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :address, :phone)
   end
 
 end
