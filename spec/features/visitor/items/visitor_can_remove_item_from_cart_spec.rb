a9722734761d6b2741022f11d1066c419ab91125
sidewinder2020 <sdwinder1@gmail.com>
2017-10-25 08:32:31 -0600

    1 failure left - Twilio - going to skip it

--- spec/features/visitor/items/visitor_can_remove_item_from_cart_spec.rb
+++ spec/features/visitor/items/visitor_can_remove_item_from_cart_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Removing an item from my cart" do
-  before(:each) do
-
-    store = create(:store)
-    category = create(:category)
-    @item = create(:item, store: store, category: category)
-
-    visit store_items_path(store_slug: store.slug)
-  end
   describe "When a visitor is viewing their cart" do
     it "they can remove an item" do
+      store = create(:store)
+      category = create(:category)
+      @item = create(:item, store: store, category: category)
+
+      visit store_items_path(store_slug: store.slug)
+
       click_on "Add to cart"
 
       click_on "Add to cart"
@@ -19,7 +17,7 @@ RSpec.feature "Removing an item from my cart" do
 
       expect(page).to have_link("Remove")
 
-      click_link("Remove")
+      click_on "Remove"
 
       expect(current_path).to eq(carts_path)
       expect(page).to have_content("Successfully removed #{@item.title} from your cart.")
