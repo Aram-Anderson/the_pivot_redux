5bc5bc02c825ca2cf774d15e7113431870fe3103
sidewinder2020 <sdwinder1@gmail.com>
2017-10-25 08:05:07 -0600

    Down to 5 failing tests - no changes to code, as of yet, only to tests

--- spec/features/visitor/items/visitor_can_increase_item_quantity_in_cart_spec.rb
+++ spec/features/visitor/items/visitor_can_increase_item_quantity_in_cart_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 feature "Visitor can increase an item's quantity in the cart" do
-  before(:each) do
+  scenario "visitor has an item in the cart and then she increases it to 2" do
 
     store = create(:store)
     category = create(:category)
     @item = create(:item, store: store, category: category)
 
-  end
-
-  scenario "visitor has an item in the cart and then she increases it to 2" do
     visit store_items_path(store_slug: store.slug)
 
     click_on "Add to cart"
 
     visit carts_path
+    
     expect(page).to have_content(@item.title)
     expect(page).to have_content(1)
 
