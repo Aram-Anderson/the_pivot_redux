9ab2267dd3be706e8acd2a9692125bd56af7a210
snayrouz <samuel.nayrouz@gmail.com>
2017-10-25 08:08:45 -0600

    Skeleton for platform_admin dashboard

---
+++ spec/features/platform_admin/platform_admin_dashboard_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "Platform Admin Dashboard" do
+  it "sees the top selling merchant for the platform" do
+
+  end
+
+  it "sees the top selling items for the platform" do
+
+  end
+
+  it "sees all inactive stores" do
+
+  end
+
+  it "sees all active stores" do
+
+  end
+
+  it "sees the store with the most items" do
+
+  end
+
+  it "sees items with the most revenue" do
+
+  end
+
+  it "sees a stores revenue by date" do
+
+  end
+
+  it "sees the customers favorite store" do
+
+  end
+end
