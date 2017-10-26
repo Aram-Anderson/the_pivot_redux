68a15aaf09130084b6a0dafcbda98acd747ee0de
snayrouz <samuel.nayrouz@gmail.com>
2017-10-26 16:15:19 -0600

    Deleted tests we no longer need

--- spec/controllers/api/v1/stores/favorite_user_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::Stores::FavoriteUserController, type: :controller do
-
-end
--- spec/controllers/most_revenue_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MostRevenueController, type: :controller do
-
-end
