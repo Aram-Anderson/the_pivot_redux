937f001c515471aa392d9024f53a2b722dc6da7d
Aram Anderson <aram.j.anderson@gmail.com>
2017-10-26 14:25:37 -0600

    Top 10 stores and top 10 items on plat dashboard

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,13 @@ class Store < ApplicationRecord
   has_many :items
   has_many :user_roles
   has_many :users, through: :user_roles
+
+  def self.highest_revenue
+    select("stores.*, sum(items.price * order_items.quantity) AS revenue")
+    .joins(items: [:order_items, :orders])
+    .merge(Order.completed)
+    .group(:id)
+    .order('revenue DESC')
+    .limit(10)
+  end
 end
